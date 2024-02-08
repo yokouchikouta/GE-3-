cd0f52508c147babe374e33bb0744f957739ce4d
yokouchi_kouta <115063415+yokouchikouta@users.noreply.github.com>
2024-02-08 23:07:37 +0900

    ３章

--- WinApp.cpp
+++ WinApp.cpp
@@ -18,9 +18,7 @@ LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 void WinApp::Initialize()
 {
-    // ウィンドウサイズ
-    const int window_width = 1280;  // 横幅
-    const int window_height = 720;  // 縦幅
+   
 
     // ウィンドウクラスの設定
    
@@ -38,7 +36,7 @@ void WinApp::Initialize()
     AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
     // ウィンドウオブジェクトの生成
-    HWND hwnd = CreateWindow(w.lpszClassName, // クラス名
+   hwnd = CreateWindow(w.lpszClassName, // クラス名
         L"DirectXGame",         // タイトルバーの文字
         WS_OVERLAPPEDWINDOW,        // 標準的なウィンドウスタイル
         CW_USEDEFAULT,              // 表示X座標（OSに任せる）
